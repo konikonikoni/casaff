aa0bf753b4bab72f9c744a867cd76b46e51aa609
Konrad Dietmann <konrad.dietmann@gmail.com>
2024-04-11 21:01:56 +0200

    Version 0.1 Files

--- casaff/settings.py
+++ casaff/settings.py
@@ -14,7 +14,7 @@ SECRET_KEY = 'django-insecure-&kd8ds#ud%^78mmii6@w9wz(v+f9r#cbbid#)8#lcmdhr4q*zj
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [3.123.254.97, 127.0.0.1]
+ALLOWED_HOSTS = ["3.123.254.97", "127.0.0.1", "localhost"]
 
 # Application definition
 
