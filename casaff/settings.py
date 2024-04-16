742001239a575265637e551c33766f1525f66648
Konrad Dietmann <konrad.dietmann@gmail.com>
2024-04-16 22:05:38 +0200

    Version 0.4 Orange - Filter changed. No checkboxes, but highlighted background instead.

--- casaff/settings.py
+++ casaff/settings.py
@@ -109,7 +109,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = '/var/www/kennekslots/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
@@ -119,4 +119,4 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 # Media settings
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = /var/www/kennekslots/media/
