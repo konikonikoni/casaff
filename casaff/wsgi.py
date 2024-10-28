7d13c2473e3f5153712edf57af67ed28309e807e
koni <152221158+konikonikoni@users.noreply.github.com>
2024-10-28 16:24:53 +0100

    Test Commit

--- casaff/wsgi.py
+++ casaff/wsgi.py
@@ -1,16 +1,13 @@
-"""
-WSGI config for casaff project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
-"""
-
 import os
+import sys
 
-from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'casaff.settings')
+sys.path.insert(0, os.path.dirname(__file__))
 
-application = get_wsgi_application()
+
+def application(environ, start_response):
+    start_response('200 OK', [('Content-Type', 'text/plain')])
+    message = 'It works!\n'
+    version = 'Python %s\n' % sys.version.split()[0]
+    response = '\n'.join([message, version])
+    return [response.encode()]
